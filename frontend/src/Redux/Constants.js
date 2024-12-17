34229f6d0ddd8235a27cac76a669cae55626a970
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-12-17 18:43:23 +0100

    Update some code

--- frontend/src/Redux/Constants.js
+++ frontend/src/Redux/Constants.js
@@ -1,14 +1,14 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
+// export const BASE_URL = "";
+// export const USERS_URL = "/api/users";
+// export const CATEGORY_URL = "/api/category";
+// export const PRODUCT_URL = "/api/products";
+// export const UPLOAD_URL = "/api/upload";
+// export const ORDERS_URL = "/api/orders";
 
-// export const BASE_URL = "https://tools-master.onrender.com";
+export const BASE_URL = "https://tools-master.onrender.com";
 
-// export const USERS_URL = `${BASE_URL}/api/users`;
-// export const CATEGORY_URL = `${BASE_URL}/api/category`;
-// export const PRODUCT_URL = `${BASE_URL}/api/products`;
-// export const UPLOAD_URL = `${BASE_URL}/api/upload`;
-// export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
